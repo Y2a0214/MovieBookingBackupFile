ed977f300ca5f11522fc549158fab06ee0a97c80
Yash Maliye <127768459+Y2a0214@users.noreply.github.com>
2024-05-19 19:38:17 +0530

    db connection

--- server/src/db/connection.js
+++ server/src/db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://localhost:27017/bookMovie")
+mongoose.connect("mongodb+srv://yash0214:yash0214@cluster0.grzeyny.mongodb.net/")
 .then(() => { console.log("connection established with mongodb server online"); })
 .catch(err => {
     console.log("error while connection", err)
