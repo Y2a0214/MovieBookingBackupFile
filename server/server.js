afa77b42cbdf42091e655c676b826c6fdf373d96
Yash Maliye <127768459+Y2a0214@users.noreply.github.com>
2024-05-19 19:24:09 +0530

    added routers

--- server/server.js
+++ server/server.js
@@ -1,45 +1,15 @@
 const express = require('express')
 require("./src/db/connection");
 const collection_connection = require("./src/modul/schema");
+const router = require('./src/routers/movieRouter');
+const port = process.env.PORT || 8080;
 
 const app = express()
 
 app.use(express.json())
 
-app.post('/booking' ,async (req, res) => {
-    try{
-        const moviesRecord = collection_connection(req.body)
-        const insertMovie = await moviesRecord.save()
-        res.status(201).send(insertMovie)
-    }catch(e){
-        res.send(e)
-        console.log(e)
-    }
-})
+app.use(router)
 
-app.get('/booking' ,async (req, res) => {
-    try{
-        const [data] = await collection_connection.find().sort({_id:-1}).limit(1)
-        
-        if(data.length == 0){
-            res.status(200).json({
-                message:"No previous Booking found!",
-                status:200,
-                data:null
-            })
-        }else{
-            res.status(200).json({
-                message:"last booking!",
-                status:200,
-                data:data
-            })
-        }
-    }catch(e){
-        res.send(e)
-        console.log(e)
-    }
-})
-
-app.listen(8080, () => {
+app.listen(port, () => {
     console.log('server is runing on port 8080')
 })
