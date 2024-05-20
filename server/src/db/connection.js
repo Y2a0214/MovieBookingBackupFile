a20896ccc73f273339d842ba84fbc630d4546a58
Yash Maliye <127768459+Y2a0214@users.noreply.github.com>
2024-05-21 01:00:42 +0530

    connected to atlas

--- server/src/db/connection.js
+++ server/src/db/connection.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose')
+const dotenv = require("dotenv");
+dotenv.config();
+const apiKey = process.env.API_KEY;
 
-mongoose.connect("mongodb+srv://yash0214:yash0214@cluster0.grzeyny.mongodb.net/")
+
+mongoose.connect(apiKey, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+})
 .then(() => { console.log("connection established with mongodb server online"); })
 .catch(err => {
     console.log("error while connection", err)
